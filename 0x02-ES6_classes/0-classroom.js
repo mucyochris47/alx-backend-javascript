2b25beaab356c648762efd412dfebd8dae2a337a
mucyochris47 <mucyochris250@gmail.com>
2025-04-18 20:19:40 -0700

    task 0

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,14 @@
+/**
+ * Represent a class room
+ */
+export default class ClassRoom {
+  /**
+   * Create a new classroom
+   * @param {Number} maxStudentsSize - The maximum number of
+   *
+   * students in the class
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
