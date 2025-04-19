02b2f8496d0ff2b445e10afba0bd76b75b8c23b1
mucyochris47 <mucyochris250@gmail.com>
2025-04-18 20:28:03 -0700

    task 6

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,24 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  /* getter and setter for floor */
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('floors must be a number');
+    }
+    this._floors = value;
+  }
+
+  evacuationWarningMessage() {
+    return (`Evacuate slowly the ${this._floors} floors`);
+  }
+}
